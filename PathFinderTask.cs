0c6a7220599118247230502e05f4ef2703b61c0f
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-07-26 16:42:37 +0300

    Second incorrect code

--- PathFinderTask.cs
+++ PathFinderTask.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-
+using System.Linq;
 
 namespace RoutePlanning
 {
@@ -9,48 +9,78 @@ namespace RoutePlanning
 	{
         public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
         {
-            if (checkpoints.Length > 1)
+            var bestOrder = MakeTrivialPermutation(checkpoints.Length);
+            MakeBestPermutation(checkpoints, bestOrder.ToArray(), bestOrder);
+            return bestOrder;
+        }
+
+        private static void MakeBestPermutation(Point[] checkpoints,
+            int[] randomPermutation, int[] bestPermutation, int position = 0)
+        {
+            if(position == randomPermutation.Length)
             {
-                var indexUsed = new List<int>() { 0 } ;
-                for (int i = 0; i < checkpoints.Length; i++)
-                {
-                    AddIndexUsed(checkpoints,
-                        indexUsed[indexUsed.Count - 1], indexUsed);
-                }
-                return indexUsed.ToArray();
+                var randomPermutationDistance =
+                    GetPathLength(checkpoints, randomPermutation);
+                var bestPermutationDistance =
+                    GetPathLength(checkpoints, bestPermutation);
+                if (randomPermutationDistance < bestPermutationDistance)
+                    bestPermutation = randomPermutation.ToArray();
+                return;
             }
             else
             {
-                return MakeTrivialPermutation(checkpoints.Length);
-            }
-		}
-
-        public static void AddIndexUsed
-            (Point[] checkpoints, int startIndex, List<int> indexUsed,
-            double minDistance = double.MaxValue, int nextIndex = 0,
-            int minIndex = 0)
-        {
-            if (nextIndex == checkpoints.Length)
-            {
-                indexUsed.Add(minIndex);
-                return;
-            }
-
-            if (!indexUsed.Contains(nextIndex))
-            {
-                var randomDistance = PointExtensions
-                    .DistanceTo(checkpoints[startIndex], checkpoints[minIndex]);
-                if (randomDistance < minDistance)
+                for (int i = 0; i < randomPermutation.Length; i++)
                 {
-                    minIndex = nextIndex;
-                    minDistance = randomDistance;
+                    var index = Array.IndexOf(randomPermutation, i, 0, position);
+                    if (index == -1)
+                    {
+                        randomPermutation[position] = i;
+                        MakeBestPermutation(checkpoints, randomPermutation,
+                            bestPermutation, position + 1);
+                    }
                 }
             }
-            AddIndexUsed(checkpoints, startIndex, indexUsed,
-                minDistance, nextIndex + 1, minIndex);
         }
 
-		private static int[] MakeTrivialPermutation(int size)
+        private static double GetPathLength(Point[] checkpoints, int[] permutation)
+        {
+            double totalDistance = 0;
+            if (permutation.Length > 1)
+            {
+                for (int i = 1; i < permutation.Length; i++)
+                {
+                    var pointA = checkpoints[permutation[i - 1]];
+                    var pointB = checkpoints[permutation[i]];
+                    totalDistance += PointExtensions
+                        .DistanceTo(pointA, pointB);
+                }
+            }
+            return totalDistance;
+        }
+
+        public static void MakePermutations(int[] permutation, int position, List<int[]> result)
+        {
+            if (position == permutation.Length)
+            {
+                result.Add(permutation.ToArray());
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < permutation.Length; i++)
+                {
+                    var index = Array.IndexOf(permutation, i, 0, position);
+                    if (index == -1)
+                    {
+                        permutation[position] = i;
+                        MakePermutations(permutation, position + 1, result);
+                    }
+                }
+            }
+        }
+
+
+        private static int[] MakeTrivialPermutation(int size)
 		{
 			var bestOrder = new int[size];
 			for (int i = 0; i < bestOrder.Length; i++)
