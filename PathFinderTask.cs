6823e97041252981103b26a592bf19b5b7163c01
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-07-26 15:29:18 +0300

    Incorrect code

--- PathFinderTask.cs
+++ PathFinderTask.cs
@@ -2,15 +2,53 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 
+
 namespace RoutePlanning
 {
 	public static class PathFinderTask
 	{
-		public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
-		{
-			var bestOrder = MakeTrivialPermutation(checkpoints.Length);
-			return bestOrder;
+        public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
+        {
+            if (checkpoints.Length > 1)
+            {
+                var indexUsed = new List<int>() { 0 } ;
+                for (int i = 0; i < checkpoints.Length; i++)
+                {
+                    AddIndexUsed(checkpoints,
+                        indexUsed[indexUsed.Count - 1], indexUsed);
+                }
+                return indexUsed.ToArray();
+            }
+            else
+            {
+                return MakeTrivialPermutation(checkpoints.Length);
+            }
 		}
+
+        public static void AddIndexUsed
+            (Point[] checkpoints, int startIndex, List<int> indexUsed,
+            double minDistance = double.MaxValue, int nextIndex = 0,
+            int minIndex = 0)
+        {
+            if (nextIndex == checkpoints.Length)
+            {
+                indexUsed.Add(minIndex);
+                return;
+            }
+
+            if (!indexUsed.Contains(nextIndex))
+            {
+                var randomDistance = PointExtensions
+                    .DistanceTo(checkpoints[startIndex], checkpoints[minIndex]);
+                if (randomDistance < minDistance)
+                {
+                    minIndex = nextIndex;
+                    minDistance = randomDistance;
+                }
+            }
+            AddIndexUsed(checkpoints, startIndex, indexUsed,
+                minDistance, nextIndex + 1, minIndex);
+        }
 
 		private static int[] MakeTrivialPermutation(int size)
 		{
---
+++ PathFinderTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace RoutePlanning
+{
+    [TestFixture]
+    class PathFinderTests
+    {
+        private static IEnumerable<TestCaseData> PathFinderCaseData
+        {
+            get
+            {
+                var checkpoints = new Point[]
+                {
+                    new Point(0, 0),
+                    new Point(1, 1),
+                    new Point(2, 2),
+                };
+                int startIndex = 0;
+                var indexUsed = new List<int>() { 0 };
+                var expectedResult = new List<int>()
+                { 0, 1};
+                yield return new TestCaseData(checkpoints, startIndex,
+                    indexUsed, expectedResult)
+                    .SetName("Simple test to near point");
+
+                indexUsed = new List<int>() { 0, 1 };
+                expectedResult = new List<int>()
+                { 0, 1, 2};
+                yield return new TestCaseData(checkpoints, startIndex,
+                    indexUsed, expectedResult)
+                    .SetName("Test with indexUsed 1");
+
+                indexUsed = new List<int>() { 0, 2 };
+                expectedResult = new List<int>()
+                { 0, 2, 1};
+                yield return new TestCaseData(checkpoints, startIndex,
+                    indexUsed, expectedResult);
+            }
+        }
+
+        [TestCaseSource("PathFinderCaseData")]
+        public void AddIndexUsedTest
+            (Point[] checkpoints, int startIndex, 
+            List<int> indexUsed, List<int> expectedResult)
+        {
+            PathFinderTask.AddIndexUsed(checkpoints, startIndex, indexUsed);
+            Assert.AreEqual(expectedResult, indexUsed);
+        }
+    }
+}
